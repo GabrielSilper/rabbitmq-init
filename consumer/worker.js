315db81134c24def46dc4ea121d01b90f0663784
GabrielSilper <gabrielsilper.main@gmail.com>
2024-01-04 18:49:51 -0400

    simulando uma fila com tarefas pt1 - by RabbitMQ docs

---
+++ consumer/worker.js
@@ -0,0 +1,33 @@
+const amqp = require('amqplib/callback_api');
+
+amqp.connect('amqp://localhost', (err0, conn) => {
+  if (err0) throw err0;
+
+  conn.createChannel((err1, chanel) => {
+    if (err1) throw err1;
+
+    const queue = 'hello';
+
+    chanel.assertQueue(queue, {
+      durable: false,
+    });
+
+    console.log(`Waiting for messages in ${queue} queue. to Exit press CTRL+C`);
+
+    chanel.consume(
+      queue,
+      (msg) => {
+        const secs = msg.content.toString().length;
+
+        console.log(
+          `New message => ${msg.content.toString()}, task duration: ${secs} seconds`
+        );
+
+        // setTimeout(() => {
+        //   console.log('Task is done!');
+        // }, secs * 1000);
+      },
+      { noAck: true }
+    );
+  });
+});
